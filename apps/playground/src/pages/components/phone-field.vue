12c5f337e06c4f36bda018013db2eac7ae2c89ce
fontanaen <enzo.fontana00@hotmail.com>
2026-01-18 22:59:04 +0100

    wip

--- apps/playground/src/pages/components/phone-field.vue
+++ apps/playground/src/pages/components/phone-field.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { PhoneField, PhoneFieldCountrySelect, PhoneFieldIndicator, PhoneFieldInput, validatePhoneNumber } from "@vuetella/registry/ui/phone-field";
+import { Switch } from "@vuetella/ui/components/switch";
 import { ToggleGroup, ToggleGroupItem } from "@vuetella/ui/components/toggle-group";
 import { ref } from "vue";
 
@@ -7,10 +8,9 @@ definePage({
   name: "components.phone-field",
 });
 
-const countryCodeFR = ref<string>("FR");
-const countryCodeUS = ref<string>("US");
-const phoneFR = ref<string>("+33771200669");
-const phoneUS = ref<string>("+12133734");
+const countryCode = ref<string>("FR");
+const phone = ref<string>("");
+const resetOnCountryChange = ref<boolean>(false);
 
 const size = ref<"sm" | "default" | "lg">("default");
 
@@ -38,24 +38,21 @@ const buttonSizes: { size: "sm" | "default" | "lg"; variant: "outline" | "defaul
       </ToggleGroupItem>
     </ToggleGroup>
 
+    <div class="flex items-center gap-2 text-sm">
+      <Switch v-model="resetOnCountryChange" /> Reset on country change
+    </div>
+
     <PhoneField 
-      v-model="phoneFR" 
-      v-model:country-code="countryCodeFR" 
+      v-model="phone" 
+      v-model:country-code="countryCode" 
       :size="size" 
       format="international" 
-      reset-on-country-change 
+      :reset-on-country-change="resetOnCountryChange" 
       :preferred-countries="['FR', 'US']" 
       :ignored-countries="['AC', 'TA', 'DE']"
     >
         <PhoneFieldCountrySelect />
         <PhoneFieldInput />
-    </PhoneField>
-
-    <PhoneField v-model="phoneUS" v-model:country-code="countryCodeUS" :size="size" format="national" :preferred-countries="['FR', 'US']" :ignored-countries="['AC', 'TA', 'DE']">
-        <PhoneFieldCountrySelect />
-        <PhoneFieldInput>
-          <PhoneFieldIndicator />
-        </PhoneFieldInput>
     </PhoneField>
   </div>
 </template>
