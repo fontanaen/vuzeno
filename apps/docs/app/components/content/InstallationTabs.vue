d97cfad8f512af18e9232f278fedb46d870a530e
fontanaen <enzo.fontana00@hotmail.com>
2026-03-22 16:42:58 +0100

    docs: update installation tabs light mode style

--- apps/docs/app/components/content/InstallationTabs.vue
+++ apps/docs/app/components/content/InstallationTabs.vue
@@ -40,7 +40,7 @@ const currentCommand = computed(() => {
   >
     <div class="bg-background border-b border-border flex px-2">
       <div class="flex items-center gap-2 w-full text-[13px]">
-        <div class="bg-black/90 text-white dark:bg-muted dark:text-black size-7 flex items-center justify-center rounded-sm">
+        <div class="bg-black/90 text-white dark:bg-muted size-7 flex items-center justify-center rounded-sm">
           <TerminalIcon class="size-4" />
         </div>
         <TabsList class="flex gap-2 bg-transparent p-2">
